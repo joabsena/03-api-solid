2ca06410325c0d63b3d3b29832ac37327799258f
Joab Sena <joabarbosa1@hotmail.com>
2024-05-22 06:52:48 -0300

    feat: create new controller to create gyms

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import fastify from 'fastify'
-import { appRoutes } from './http/router'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import { userRoutes } from './http/controllers/users/router'
+import { gymRoutes } from './http/controllers/gyms/router'
 
 export const app = fastify()
 
@@ -10,7 +11,8 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
 
-app.register(appRoutes)
+app.register(userRoutes)
+app.register(gymRoutes)
 
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
